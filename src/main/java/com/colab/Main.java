1b275a7d53a1da020ab6b1ccf01038553032ad6f
isaiah vang <vang.isaiah3@gmail.com>
2023-05-30 11:11:11 -0400

    modified all 3 classes

--- src/main/java/com/colab/Main.java
+++ src/main/java/com/colab/Main.java
@@ -4,6 +4,38 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        Scanner scanner = new Scanner(System.in);
+
+        int optionInput;
+
+        do {
+            System.out.println("Here are your options: \n" +
+                    "\t1 - Place a New Order\n" +
+                    "\t2 - Exit\n" +
+                    "Enter which one you would like: \n"
+            );
+            optionInput = scanner.nextInt();
+
+            switch (optionInput) {
+                case 1:
+                    System.out.println("something smells fishy");
+                    break;
+                case 2:
+                    System.out.println("Have a wonderful day!");
+                    break;
+                default:
+                    System.out.println("Error, that option is invalid.");
+                    break;
+            }
+        } while (optionInput != 2);
+
+
+
+
+
+
+//        Beverage beverage = new Beverage("small", "sweet tea", 3.99f);
+//        System.out.println(beverage);
     }
 }
